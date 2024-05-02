500df795c2cc37ca92071377034f4bafbfc79d19
R-enanVieira <renanv.guedes7@gmail.com>
2024-05-01 21:23:52 -0300

    starting parser

--- lightsout.cpp
+++ lightsout.cpp
@@ -10,9 +10,37 @@ typedef vector<int> vi;
 #define pb push_back
 #define ln "\n"
 
+vector<string> matrix;
+map<string, string> predicates{
+    {"W", "is-off "},
+    {"R", "is-red "},
+    {"G", "is-green "},
+    {"B", "is-blue "},
+};
+
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
+
+  string row;
+
+  while (cin >> row) matrix.pb(row);
+
+  FILE *problem = fopen("problem.pddl", "w"),
+       *domain = fopen("domain.pddl", "w");
+
+  fprintf(problem,
+          "(define (problem lights-out)\n\t(:domain lights-out)\n\t(:objects");
+
+  int m_lenght = matrix.size();
+  for (int i = 0; i < m_lenght; i++)
+    for (int j = 0; j < matrix[i].size() / 2; j++)
+      fprintf(problem, " p%d-%d", i, j);
+
+  fprintf(problem, " - position)\n\t(:init\n");
+
+  fclose(domain), fclose(problem);
+  system("/home/software/planners/madagascar/M -Q domain.pddl problem.pddl");
 
   return 0;
 }
